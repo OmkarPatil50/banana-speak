fb5fed2847f958634ed68831c13042e86defdd62
Omkar Patil <Omkarp5429@gmail.com>
2022-06-20 16:55:48 +0530

    final commit

--- code.js
+++ code.js
@@ -1,10 +1,33 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var txtInput = document.querySelector("#txt-input")
 var txtOutput = document.querySelector("#txt-output")
+var url = 	"https://api.funtranslations.com/translate/minion.json"
+
+function readURl(text)
+{   var text = txtInput.value ;
+    return url + "?" + "text=" + text;
+}
 
 
-function clickEventHandler(){
-    txtOutput.innerText= "asdoiasjd" + txtInput.value}
+function errorHandler(error)
+{
+ console.log("error occured", error)
+ alert("Something wrong with server. Please try again after some time!")
+} 
+
+function clickEventHandler()
+{
+    fetch(readURl(txtInput))
+    .then(response => response.json())
+    .then(json => {
+        var translatedOutput = json.contents.translated;
+        txtOutput.innerText = translatedOutput;
+    })
+    .catch(errorHandler)
+
+};
+
+
    
 
 
